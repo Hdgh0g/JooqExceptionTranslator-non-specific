19b535781ec2f199d1e42896fea1da1426f5c89a
Aleksandr Krylov <aleksandr.krylov@wolt.com>
2023-02-17 10:26:36 +0200

    Initial commit

---
+++ src/main/kotlin/com/hdgh0g/jooq/translator/reproduce/JooqExceptionTranslatorNonSpecificApplication.kt
@@ -0,0 +1,11 @@
+package com.hdgh0g.jooq.translator.reproduce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JooqExceptionTranslatorNonSpecificApplication
+
+fun main(args: Array<String>) {
+	runApplication<JooqExceptionTranslatorNonSpecificApplication>(*args)
+}
---
+++ src/test/kotlin/com/hdgh0g/jooq/translator/reproduce/JooqExceptionTranslatorNonSpecificApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hdgh0g.jooq.translator.reproduce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JooqExceptionTranslatorNonSpecificApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
